9600f283b93ca4dbf09ea91fa8bcabd805597796
Moaz bin Mokhtar <moaz.mokhtar@gmail.com>
2023-07-31 23:13:53 +0300

    add installing libssl-dev

--- install_dev_dep.sh
+++ install_dev_dep.sh
@@ -4,6 +4,7 @@
 echo "Installing development packages dependencies"
 apt-get update
 apt-get install -y --no-install-recommends \
+  libssl-dev \
   build-essential \
   ca-certificates \
   clang \
